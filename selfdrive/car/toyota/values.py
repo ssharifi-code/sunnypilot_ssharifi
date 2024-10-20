f5cfec531c20a267df200a00fac35fa29d7b1e28
ssharifi-code <142284672+ssharifi-code@users.noreply.github.com>
2024-10-19 20:16:25 -0600

    Update values.py

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -312,7 +312,7 @@ class CAR(Platforms):
   LEXUS_LC_TSSP = PlatformConfig(
     [ToyotaCarDocs("Lexus LC 2018")],
     CarSpecs(mass=4500. * CV.LB_TO_KG, wheelbase=2.87, steerRatio=13.0, tireStiffnessFactor=0.444),
-    dbc_dict('toyota_new_mc_pt_generated', 'toyota_adas'),
+    dbc_dict('toyota_tnga_k_pt_generated', 'toyota_adas'),
     flags=ToyotaFlags.UNSUPPORTED_DSU,
   )
   LEXUS_LC_TSS2 = ToyotaTSS2PlatformConfig(
